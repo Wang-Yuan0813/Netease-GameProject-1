091e0fdeb5b913d14a809df4a454e57d2c0fa739
wangyuan <wy2283809826@gmail.com>
2025-11-14 02:12:55 +0000

    upload files

---
+++ custom/custom_editor_sequence.py
@@ -0,0 +1,5 @@
+# -*- encoding:utf-8 -*-
+_reload_all = True
+
+
+data = {134281340: {'path': u'custom_ui/18', 'describe': '', 'name': u'18', 'key': 134281340, 'uid': 'editor_sequence_134281340', 'is_custom': True, 'plist_name': u'18'}}
